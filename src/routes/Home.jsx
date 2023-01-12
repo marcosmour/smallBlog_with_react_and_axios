855f996b3ef45c424eb598ef94fba6f85db7e1d1
Marcos Moura <marcosmoura201@hotmail.com>
2023-01-11 21:16:20 -0300

    Enviando o projeto small blog com react e axios

---
+++ src/routes/Home.jsx
@@ -0,0 +1,44 @@
+import blogFetch from "../axios/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './Home.css'
+
+const Home = () => {
+
+  const [post, setPost] = useState([])
+
+  const getPost = async () => {
+    try {
+      
+      const response = await blogFetch.get("/posts")
+        
+        const data = response.data
+        setPost(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getPost()
+  },[])
+
+  return (
+    <div className="home">
+      <h1>Ultimos Posts</h1>
+      {post.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        post.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Home
